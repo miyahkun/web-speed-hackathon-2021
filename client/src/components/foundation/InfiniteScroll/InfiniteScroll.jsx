43f3de15736d8515931e632ad65368065143d809
Keiji Miyazaki <ke.miyazaki@raksul.com>
2021-12-26 21:15:31 +0900

    Throttle scroll handler

--- client/src/components/foundation/InfiniteScroll/InfiniteScroll.jsx
+++ client/src/components/foundation/InfiniteScroll/InfiniteScroll.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { throttle } from 'lodash-es';
 
 /**
  * @typedef {object} Props
@@ -7,6 +8,9 @@ import React from 'react';
  * @property {() => void} fetchMore
  */
 
+const HEIGHT_THRESHOLD = 200;
+const WAIT_MILLI_SEC = 300;
+
 /** @type {React.VFC<Props>} */
 const InfiniteScroll = ({ children, fetchMore, items }) => {
   const latestItem = items[items.length - 1];
@@ -14,38 +18,40 @@ const InfiniteScroll = ({ children, fetchMore, items }) => {
   const prevReachedRef = React.useRef(false);
 
   React.useEffect(() => {
-    const handler = () => {
-      // FIXME:
-      const hasReached = () => {
-        return window.innerHeight + Math.ceil(window.scrollY) >= document.body.offsetHeight;
-      };
+    const handler = throttle(
+      () => {
+        const hasReached =
+          window.innerHeight + Math.ceil(window.scrollY) >= document.body.offsetHeight - HEIGHT_THRESHOLD;
 
-      // 画面最下部にスクロールしたタイミングで、登録したハンドラを呼び出す
-      if (hasReached && !prevReachedRef.current) {
-        // アイテムがないときは追加で読み込まない
-        if (latestItem !== undefined) {
-          fetchMore();
+        // 画面最下部にスクロールしたタイミングで、登録したハンドラを呼び出す
+        if (hasReached && !prevReachedRef.current) {
+          // アイテムがないときは追加で読み込まない
+          if (latestItem !== undefined) {
+            fetchMore();
+          }
         }
-      }
 
-      prevReachedRef.current = hasReached;
-    };
+        prevReachedRef.current = hasReached;
+      },
+      WAIT_MILLI_SEC,
+      { leading: false },
+    );
 
     // 最初は実行されないので手動で呼び出す
     prevReachedRef.current = false;
     handler();
 
     // FIXME:
-    // document.addEventListener('wheel', handler, { passive: false });
-    // document.addEventListener('touchmove', handler, { passive: false });
-    // document.addEventListener('resize', handler, { passive: false });
-    // document.addEventListener('scroll', handler, { passive: false });
+    document.addEventListener('wheel', handler, { passive: false });
+    document.addEventListener('touchmove', handler, { passive: false });
+    document.addEventListener('resize', handler, { passive: false });
+    document.addEventListener('scroll', handler, { passive: false });
     return () => {
       // FIXME:
-      // document.removeEventListener('wheel', handler);
-      // document.removeEventListener('touchmove', handler);
-      // document.removeEventListener('resize', handler);
-      // document.removeEventListener('scroll', handler);
+      document.removeEventListener('wheel', handler);
+      document.removeEventListener('touchmove', handler);
+      document.removeEventListener('resize', handler);
+      document.removeEventListener('scroll', handler);
     };
   }, [latestItem, fetchMore]);
 
