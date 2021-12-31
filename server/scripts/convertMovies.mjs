32619ea3e9ed10a35d9e1be21e7fd44a07d688b9
Keiji Miyazaki <ke.miyazaki@raksul.com>
2021-12-31 16:46:10 +0900

    Convert movies to webm

---
+++ server/scripts/convertMovies.mjs
@@ -0,0 +1,65 @@
+import fs from 'fs/promises';
+import path from 'path';
+import process from 'process';
+import { createFFmpeg, fetchFile } from '@ffmpeg/ffmpeg';
+
+const MOVIES_DIR = path.resolve(process.cwd(), 'public', 'movies');
+const EXTENSION = 'webm';
+
+const ffmpeg = createFFmpeg({ log: true });
+
+const output = (basename) => `${basename}.${EXTENSION}`;
+
+const asyncGen = async function* (promises) {
+  for (const promise of promises) {
+    yield await promise();
+  }
+};
+
+const init = async () => {
+  const basenameList = (await fs.readdir(MOVIES_DIR)).map((file) => path.basename(file, '.gif'));
+
+  await ffmpeg.load();
+
+  const result = [];
+
+  const promises = basenameList.map((basename) => {
+    return async () => {
+      const source = path.resolve(MOVIES_DIR, `${basename}.gif`);
+      const target = path.resolve(MOVIES_DIR, output(basename));
+
+      ffmpeg.FS('writeFile', basename, await fetchFile(source));
+
+      await ffmpeg.run(
+        '-i',
+        basename,
+        '-crf',
+        '25',
+        '-c:v',
+        'vp9',
+        '-vf',
+        'scale=600:-1',
+        '-movflags',
+        '+faststart',
+        '-an',
+        output(basename),
+      );
+      await fs.writeFile(target, ffmpeg.FS('readFile', output(basename)));
+
+      ffmpeg.FS('unlink', basename);
+      ffmpeg.FS('unlink', output(basename));
+    };
+  });
+
+  console.log('promises', promises);
+
+  for await (const res of asyncGen(promises)) {
+    result.push(res);
+  }
+
+  console.log('result', result);
+
+  process.exit(0);
+};
+
+init();
